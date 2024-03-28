a4af7d36bbaa49c90ab2b15c7cc77145901b0dab
nestorjunior <contato.nestor@gmail.com>
2024-03-28 17:37:38 -0300

    fix: footer bg color

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,6 +1,8 @@
+import './FooterComponent.scss'
+
 export function FooterComponent() {
   return(
-    <footer className="footer bg-dark text-light text-center py-3">
+    <footer className="footer footer-wishorama text-center py-3">
       <div className="container">
         <span>Wishorama® 2024, All rights reserved.</span>
       </div>
--- src/components/LogoComponent.tsx
+++ src/components/LogoComponent.tsx
@@ -1,3 +1,5 @@
+import './LogoComponent.scss'
+
 export function LogoComponent() {
   return(
     <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
