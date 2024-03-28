bcf4c92740ce0c0615b3cad7f6044c78b7ca73ed
nestorjunior <contato.nestor@gmail.com>
2024-03-28 09:08:01 -0300

    initial setup project structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap"
+
+function App() {
+  return (
+    <>
+      <h3>Wishorama®</h3>
+      <Button variant="primary">Primary button</Button>
+    </>
+  )
+}
+
+export default App
