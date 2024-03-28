c6094a82f3198e1fb05d9cfd6acf87adf541f07c
nestorjunior <contato.nestor@gmail.com>
2024-03-28 17:50:03 -0300

    fix: button width space

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -3,18 +3,20 @@ import { SearchComponent } from './SearchComponent'
 import { LogoComponent } from './LogoComponent'
 
 import './NavbarComponent.scss'
+import { CiUser } from "react-icons/ci";
+
 
 export function NavbarComponent() {
   return(
     <>
-      <nav className="navbar bg-body-tertiary">
+      <nav className="navbar navbar-wishorama bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand me-2" href="#">
             <LogoComponent />
           </a>
           <SearchComponent />
           <div>
-            <ButtonComponent />
+            <CiUser size={34} />
             <ButtonComponent />
           </div>
         </div>
