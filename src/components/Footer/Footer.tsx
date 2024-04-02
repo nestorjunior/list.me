f84d9846303d4abadcebd9644c1e421fef48db11
Nestor Júnior <nestorjunior@Nestors-MacBook-Air.local>
2024-04-02 13:44:41 -0300

    update: footer fixed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import './Footer.scss'
 
 export function Footer() {
   return(
-    <footer className="footer mt-auto py-3 bg-light fixed-bottom">
+    <footer className="footer mt-auto py-3 bg-light">
       <div className="container text-center">
 				<span>Wishbox® 2024, All rights reserved.</span>
       </div>
