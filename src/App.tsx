5ab3dc9c4dabead9d625c06e4481bed514c6512c
Nestor Júnior <nestorjunior@Nestors-MacBook-Air.local>
2024-04-04 16:20:04 -0300

    Update create page account + component: create account

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import LoginPage from './pages/LoginPage'
+import CreateAccount from './pages/CreateAccount'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
 						<Route path='/login' element={<LoginPage />} />
+						<Route path='/account' element={<CreateAccount />} />
           </Routes>
         </div>
       </Router>
---
+++ src/pages/CreateAccount.tsx
@@ -0,0 +1,11 @@
+import Account from "../components/Account/Account"
+
+const LoginPage = () => {
+	return(
+		<>
+			<Account />
+		</>
+	)
+}
+
+export default LoginPage
