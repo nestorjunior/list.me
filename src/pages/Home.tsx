a31e0379ca9ad20a714d68f4142119cc53de0360
Nestor Júnior <nestorjunior@Nestors-MacBook-Air.local>
2024-04-17 20:41:21 -0300

    cleanup: remove css unnecessary

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+// import { CardHeader } from '../components/CardHeader/CardHeader';
+// import { CardProducts } from '../components/CardProducts/CardProducts';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
-
-import { Footer } from '../components/Footer/Footer';
-import { CardHeader } from '../components/CardHeader/CardHeader';
-import { CardProducts } from '../components/CardProducts/CardProducts';
 import { Navbar } from '../components/Navbar/Navbar';
-
-import './Home.scss';
 import { Header } from '../components/Header/Header';
 import { Main } from '../components/Main/Main';
+import { Footer } from '../components/Footer/Footer';
+
+import './Home.scss';
 
 export function Home() {
   return (
     <>
-      <div className="container-wb container-fluid pt-0">
+      <div className="wbox-main-container container-fluid">
         <Navbar />
         <Header />
         <Main />
