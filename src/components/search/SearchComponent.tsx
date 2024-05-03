061e8d6251eb4ec8e1518d73ff8b84289ebf37a2
Nestor Júnior <nestorjunior@Nestors-MacBook-Air.local>
2024-05-03 12:13:02 -0300

    remove: component Search not used

--- src/components/search/SearchComponent.tsx
+++
@@ -1,9 +0,0 @@
-import './SearchComponent.scss'
-
-export function SearchComponent() {
-  return(
-    <form className="d-flex align-items-center flex-grow-1">
-      <input className="form-control me-2 flex-grow-1 centered-placeholder search-wishorama" type="search" placeholder="Search your list" aria-label="Search your list" />
-    </form>
-  )
-}
