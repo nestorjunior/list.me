5147931378eb691565ca6fd83ef0d404f67e132a
Nestor Júnior <nestorjunior@Nestors-MacBook-Air.local>
2024-05-03 11:43:11 -0300

    remove: component Info not used

--- src/components/Info/Info.tsx
+++
@@ -1,8 +0,0 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
-
-export function Info() {
-	return (
-		<FontAwesomeIcon icon={faCircleInfo} />
-	)
-}
