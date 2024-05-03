bd4fc5c3ca2b34ce28ee17d5a97793289b19f852
Nestor Júnior <nestorjunior@Nestors-MacBook-Air.local>
2024-05-03 12:11:26 -0300

    remove: Component Message not used

--- src/components/Message/Message.tsx
+++
@@ -1,9 +0,0 @@
-import './Message.scss'
-
-export function Message () {
-	return(
-		<>
-			<a className="btn btn-primary mx-1 btn-wishorama btn-wishorama-message" href="#">Message</a>
-		</>
-	)
-}
