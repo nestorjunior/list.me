797a5c76b1378923decacae2673b33060e167e9d
Nestor Júnior <nestorjunior@Nestors-MacBook-Air.local>
2024-05-18 09:19:19 -0300

    feat: include footer links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,15 @@ import { FooterContainer } from "./styles"
 export function Footer() {
   return(
     <FooterContainer className="footer mt-auto py-3 bg-light">
-      <div className="container text-center">
+			<hr/>
+      <div className="container text-center" style={{ display: 'flex', flexDirection: 'column' }}>
+				<span>
+					<a href="#" style={{marginRight: '5px'}}>About</a>
+					<a href="#" style={{marginRight: '5px'}}>Help Center</a>
+					<a href="#" style={{marginRight: '5px'}}>Service Terms</a>
+					<a href="#" style={{marginRight: '5px'}}>Privacy Policy</a>
+					<a href="#" style={{marginRight: '5px'}}>Cookies policy</a>
+				</span>
 				<span>Wishbox 2024, All rights reserved.</span>
       </div>
     </FooterContainer>
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -3,5 +3,13 @@ import styled from 'styled-components'
 export const FooterContainer = styled.footer`
 	margin-top: auto;
 	color: var(--gray-200);
+	font-size: 14px;
+	a {
+		color: var(--gray-200);
+		text-decoration: none;
+		&:hover{
+			text-decoration: underline;
+		}
+	}
 `
 
