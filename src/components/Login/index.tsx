d3ab647fdc29dfd5e906e1c93d5d3f81c36c3e10
Nestor Júnior <nestorjunior@Nestors-MacBook-Air.local>
2024-05-19 16:58:00 -0300

    refactored: component signup v2

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,5 +1,5 @@
 import React, { FormEvent, useState } from 'react';
-import { Box, Button, TextField, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography, IconButton } from '@mui/material';
 import { Google as GoogleIcon, Instagram as InstagramIcon } from '@mui/icons-material';
 
 export function Login () {
@@ -70,28 +70,27 @@ export function Login () {
 
   return (
 		<>
-			<Box component="form" sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%', maxWidth: '400px', backgroundColor: '#f4f5f6', padding: '20px', borderRadius: '25px', border: '6px solid var(--white-100)', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',}} onSubmit={handleSubmit}>
-				<Typography variant="h5" align="center" sx={{ mt: 0 }}> 
-					Login
-				</Typography>
-      	
-				<TextField label="Email" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)} fullWidth required />
-				<TextField label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} variant="outlined" fullWidth required />
+			<Box component="form" sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%', maxWidth: '400px', backgroundColor: '#f4f5f6', padding: '5px', borderRadius: '25px', border: '6px solid var(--white-100)', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)'}} onSubmit={handleSubmit}>
+				<Typography variant="h5" align="center">Login</Typography>
+				
+				<TextField value={email} onChange={(e) => setEmail(e.target.value)} label="Email" variant="outlined" fullWidth required/>
+				<TextField value={password} onChange={(e) => setPassword(e.target.value)} label="Senha" type="password" variant="outlined" fullWidth required />
 				{error && <div className="error-message">{error}</div>}
-				<Button type="submit" variant="contained" color="primary" fullWidth>Login</Button>
-			
+				<Button type="submit" variant="contained" color="primary" fullWidth>Entrar</Button>
+				
 				<Typography variant="body2" align="center" sx={{ mt: 0 }}>
 					<a href="#">Forgot password?</a>
 				</Typography>
-			
-				<Button variant="contained" color="primary" fullWidth startIcon={<GoogleIcon />} sx={{ backgroundColor: '#4285F4', color: '#fff', '&:hover': { backgroundColor: '#357ae8' } }} onClick={() => { console.log('Login Google')}}>
-					Sign in with Google
-				</Button>
-      
-				<Button variant="contained" color="secondary" fullWidth startIcon={<InstagramIcon />} sx={{ backgroundColor: '#E1306C', color: '#fff', '&:hover': { backgroundColor: '#c1265c' } }} onClick={() => { console.log('Login Instagram')}}>
-					Sign in with Instagram
-				</Button>
-    	</Box>
+				
+				<Box sx={{ display: 'flex', justifyContent: 'center', gap: 1 }}>
+					<IconButton color="primary" onClick={() => { console.log('login com Google')}}>
+						<GoogleIcon />
+					</IconButton>
+					<IconButton color="secondary" onClick={() => { console.log('Login com instagram')}}>
+						<InstagramIcon />
+					</IconButton>
+				</Box>
+			</Box>
 		</>
   );
 };
--- src/components/Signup/index.tsx
+++ src/components/Signup/index.tsx
@@ -1,5 +1,5 @@
 import React, { FormEvent, useState } from 'react';
-import { Box, Button, TextField, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography, IconButton } from '@mui/material';
 import { Google as GoogleIcon, Instagram as InstagramIcon } from '@mui/icons-material';
 
 
@@ -38,35 +38,34 @@ export function Signup() {
 
 	return (
 		<>
-			<Box component="form" sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%', maxWidth: '400px', backgroundColor: '#f4f5f6', padding: '20px', borderRadius: '25px', border: '6px solid var(--white-100)', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',}} onSubmit={handleSubmit}>
-				<Typography variant="h5" align="center" sx={{ mt: 0 }}>
-					Signup
-				</Typography>
+			<Box component="form" sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%', maxWidth: '400px', backgroundColor: '#f4f5f6', padding: '5px', borderRadius: '25px', border: '6px solid var(--white-100)', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)'}} onSubmit={handleSubmit}>
+				<Typography variant="h5" align="center">Signup</Typography>
 				
-				<TextField label="Email" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)} fullWidth required />
-				<TextField label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} variant="outlined" fullWidth required />
+				<TextField value={email} onChange={(e) => setEmail(e.target.value)} label="Email" variant="outlined" fullWidth required/>
+				<TextField value={password} onChange={(e) => setPassword(e.target.value)} label="Password" type="password" variant="outlined" fullWidth required />
 				<TextField label="Confirm Password" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} variant="outlined" fullWidth required />
 
 				{error && <div className="error-message">{error}</div>}
-
 				<Button type="submit" variant="contained" color="primary" fullWidth>Register</Button>
-			
+				
 				<Typography variant="body2" align="center" sx={{ mt: 0 }}>
 					<a href="#">Already have an account? Log in.</a>
 				</Typography>
+
+				<Box sx={{ display: 'flex', justifyContent: 'center', gap: 1 }}>
+					<IconButton color="primary" onClick={() => { console.log('login com Google')}}>
+						<GoogleIcon />
+					</IconButton>
+					<IconButton color="secondary" onClick={() => { console.log('Login com instagram')}}>
+						<InstagramIcon />
+					</IconButton>
+				</Box>
 
 				<Typography variant="body2" align="center" sx={{ mt: 0 }}>
 					By registering, you agree to our<br/>
 					<a href="#">Terms, Privacy Policy and Cookies Policy.</a>
 				</Typography>
-			
-				<Button variant="contained" color="primary" fullWidth startIcon={<GoogleIcon />} sx={{ backgroundColor: '#4285F4', color: '#fff', '&:hover': { backgroundColor: '#357ae8' } }} onClick={() => { console.log('Login Google')}}>
-					Sign in with Google
-				</Button>
-			
-				<Button variant="contained" color="secondary" fullWidth startIcon={<InstagramIcon />} sx={{ backgroundColor: '#E1306C', color: '#fff', '&:hover': { backgroundColor: '#c1265c' } }} onClick={() => { console.log('Login Instagram')}}>
-					Sign in with Instagram
-				</Button>
+
 			</Box>
 		</>
 	)
