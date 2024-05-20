d258edf3709635021d32f9eefd36521209a03917
Nestor Júnior <nestorjunior@Nestors-MacBook-Air.local>
2024-05-20 00:55:11 -0300

    cleanup: call react compoennts

--- src/components/Signup/index.tsx
+++ src/components/Signup/index.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { Box, Button, TextField, Typography, IconButton } from '@mui/material';
 import { Google as GoogleIcon, Instagram as InstagramIcon } from '@mui/icons-material';
 
