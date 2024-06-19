f92d287c2fe882756ef6ab4983b63bac924d0c4e
Nestor Júnior <nestorjunior@MacBook-Air-de-Nestor.local>
2024-06-19 10:00:25 -0300

    cleanup: biome adjust code

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import { GlobalStyles } from "./styles/GlobalStyles";
 
+// biome-ignore lint: Vite default code line
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<GlobalStyles />
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@mui/styled-engine': '@mui/styled-engine-sc',
-      '@fortawesome/react-fontawesome': '@fortawesome/react-fontawesome',
-      '@fortawesome/free-solid-svg-icons': '@fortawesome/free-solid-svg-icons',
-    },
-  },
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@mui/styled-engine": "@mui/styled-engine-sc",
+			"@fortawesome/react-fontawesome": "@fortawesome/react-fontawesome",
+			"@fortawesome/free-solid-svg-icons": "@fortawesome/free-solid-svg-icons",
+		},
+	},
+});
