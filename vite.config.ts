9b5fedb1559eb816dc16d10773befe8ac135bdff
Nestor Júnior <contato.nestor@gmail.com>
2024-11-20 11:02:47 -0300

    Atualizar para novo nome de repositório

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/list.me/", // Substitua pelo nome do repositório no GitHub
+  base: "/listme/",
   resolve: {
     alias: {
       "@mui/styled-engine": "@mui/styled-engine-sc",
