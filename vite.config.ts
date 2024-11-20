f566a5a9c23954a44c75e65d7230d3a9f4ccfe50
Nestor Júnior <contato.nestor@gmail.com>
2024-11-20 11:08:55 -0300

    config: vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/listme/",
+  base: "listme",
   resolve: {
     alias: {
       "@mui/styled-engine": "@mui/styled-engine-sc",
